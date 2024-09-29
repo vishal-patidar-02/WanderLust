853123b1e4173c73d18847be6392b68ac87491f7
vishal-patidar-02 <vishalpatidar07070707@gmail.com>
2024-09-29 21:00:53 +0530

    authentication

---
+++ router/user.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
+
+router.get("/signup", (req, res) => {
+  res.render("users/signup.ejs");
+});
+
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      let { email, username, password } = req.body;
+      const newUser = new User({ email, username });
+      let registerUser = await User.register(newUser, password);
+      console.log(registerUser);
+      req.flash("success", "Welcome to the wanderLust");
+      res.redirect("/listings");
+    } catch (err) {
+      req.flash("error", err.message);
+      res.redirect("/signup");
+    }
+  })
+);
+
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  async (req, res) => {
+    req.flash("success", "Welcome back to the wanderLust!");
+    res.redirect("/listings");
+  }
+);
+
+module.exports = router;
